6582123d683cdea6aaae869adcd3b7f9597999d0
nimikevin <97411659+nimikevin@users.noreply.github.com>
2022-08-03 06:50:44 -0400

    car job

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'job_card.dart';
 //mport 'home_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,12 @@ class HomePage extends StatefulWidget {
 
 // This widget is the root of your application.
 class _HomePageState extends State<HomePage> {
+
+  final List jobsForyou =[
+    ['uber', 'UI Designer', 'icons/uber.png', 45],
+    ['Google', 'Product Dev', 'icons/google.png', 88],
+    ['Apple', 'Software Eng', 'icons/logo-apple.png', 95],
+  ];
 //page controleur
 
   //final _controller = PageController();
@@ -46,59 +53,88 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           SizedBox(height: 25),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(
+                            Icons.search,
+                            size: 40,
+                            color: Colors.grey[600],
+                          ),
+                          hintText: 'cherche ton job..',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  height: 50,
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[800],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Image.asset(
+                    'icons/preferences.png',
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 50),
 
 
           Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              
-                child:
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.grey[200],
-                                border: Border.all(color:Colors.white),
-                                borderRadius: BorderRadius.circular(12)
-                                
-                                ),
-                              child: Expanded(
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    prefixIcon: Icon(
-                                      Icons.search,
-                                      size: 40,
-                                      color: Colors.grey[600],
-                                    ),
-                                    hintText: 'cherche ton job..',
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
+              padding: const EdgeInsets.only(left: 25.0),
+              child: Text(
+                'For you',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+              )),
 
-                          SizedBox(width: 10),
+               SizedBox(height: 25),
 
-                          Container(
-                            height: 50,
-                            padding: EdgeInsets.all(12),
-                            decoration: BoxDecoration(
-                              color: Colors.grey[800],
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: Image.asset('icons/preferences.png',
-                            color: Colors.white,),
-                          )
-                        ],
-                      ),
+               Container(
+                height: 160,
+                child: 
+               ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 3,
+                itemBuilder: (context,index){
+                  return JobCard(
+                    companyName: jobsForyou[index][0],
+                     jobTitle: jobsForyou[index][1],
+                      logoImagePath: jobsForyou[index][2],
+                       hourlyRate: jobsForyou[index][3],
 
-                      
-                    
-                  
-                
-              
-              ),
+                  );
+
+                })
+               ),
+
+                SizedBox(height: 50),
+
+
+          Padding(
+              padding: const EdgeInsets.only(left: 25.0),
+              child: Text(
+                'Recently Added',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+              )),
+
+
         ],
       ),
     );
---
+++ lib/job_card.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class JobCard extends StatelessWidget {
+  final String companyName;
+  final String jobTitle;
+  final String logoImagePath;
+  final int hourlyRate;
+
+  const JobCard({
+    Key? key,
+    required this.companyName,
+    required this.jobTitle,
+    required this.logoImagePath,
+    required this.hourlyRate,
+    //required this.color,
+  }) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Container(
+            width: 200,
+            padding: const EdgeInsets.all(12.0),
+            color: Colors.grey[200],
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 60,
+                      child: Image.asset(logoImagePath),
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(5),
+                      child: Container(
+                        padding: EdgeInsets.all(8),
+                        child: Text(
+                          'Part Time',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        color: Colors.grey[500],
+                    
+                    
+                      ),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top:8.0),
+                  child: Text(jobTitle,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20
+                  ),),
+                ),
+                Text('\$' + hourlyRate.toString()+ '/hr')
+              ],
+            )),
+      ),
+    );
+  }
+}
